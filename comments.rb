f0439d5a110dcefd369b4539be9f1983cbdf1d4b
kmandawe <kdmandawe06@gmail.com>
2022-03-24 15:14:24 +0800

    Comments

---
+++ comments.rb
@@ -0,0 +1,22 @@
+# This is a comment
+
+
+puts "Hello world" # Prints out "Hello world" to system out
+
+# In this section of code,
+# we are going to do operations
+# A, B, and C
+
+=begin
+puts 1 + 1
+puts 5 + 3
+puts "Hello world"
+p "Blah blah blah"
+=end
+
+puts 1 + 1
+puts 5 + 3
+puts "Hello world"
+p "Blah blah blah"
+
+
