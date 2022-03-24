9960635cb21680be7c29c6a7f5287ae9f0832070
kmandawe <kdmandawe06@gmail.com>
2022-03-24 16:23:05 +0800

    Intro to Object Methods

---
+++ object_methods.rb
@@ -0,0 +1,20 @@
+p "hello world".length
+p "hello world".upcase
+
+foo = "hello world"
+p foo.length
+p foo.upcase
+
+puts
+
+p 10.next
+p -1.next
+
+puts
+
+puts "Hello world"
+puts "Hello world".inspect
+p "Hello world"
+
+
+
