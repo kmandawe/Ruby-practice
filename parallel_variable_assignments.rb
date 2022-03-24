89f8924ce3671fdfac44b2e7d7e7ef5ee88a2bdd
kmandawe <kdmandawe06@gmail.com>
2022-03-24 15:59:30 +0800

    Parallel Variable Assignment and Swapping Variable Values

---
+++ parallel_variable_assignments.rb
@@ -0,0 +1,29 @@
+# Parallel Variable Assignment
+
+# a = 10
+# b = 20
+# c = 30
+
+a, b, c = 10, 20, 30
+
+p a
+p b
+p c
+
+p a, b, c
+puts a, b, c
+print a, b, c
+puts
+
+puts "Swapping"
+
+a = 1
+b = 2
+p a, b
+
+a, b = b, a
+
+p a, b
+
+
+
