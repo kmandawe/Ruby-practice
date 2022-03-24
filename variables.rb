7f8014771c33de1a0fc6e16248134df13619e89d
kmandawe <kdmandawe06@gmail.com>
2022-03-24 15:42:48 +0800

    Variables

---
+++ variables.rb
@@ -0,0 +1,23 @@
+name = "Boris"
+last_name = "Paskhaver"
+handsome = true
+age = 24 + 1
+
+puts name
+puts last_name
+puts name + " " + last_name
+
+puts handsome
+
+puts age + 5
+
+age = 50
+
+puts age
+
+favorite_number = 24
+p favorite_number
+
+favorite_number = "twenty-four"
+p favorite_number
+
