bb090513537aa5a22f959da305656ed9566dfa96
kmandawe <kdmandawe06@yahoo.com.ph>
2022-03-31 09:15:28 +0800

    Convert numbers to Strings and vice versa

---
+++ convert_number_to_strings.rb
@@ -0,0 +1,31 @@
+str = "5"
+p str
+p str.class
+
+p str.to_i.class
+
+p str.to_f
+p str.to_f.class
+
+number = 10
+p number.class
+p number.to_s
+p number.to_s.class
+
+p number.to_f
+p number.to_f.class
+
+pi = 3.14
+
+p pi.class
+
+p pi.to_i.class
+
+p pi.to_s
+p pi.to_s.class
+
+p "5".to_s.class
+
+p 10.to_i.class
+
+p 99.99.to_f.class
