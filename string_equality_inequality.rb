afd3c12c449b14d1e870833f862c536b75aeb574
kmandawe <kdmandawe06@yahoo.com.ph>
2022-04-02 18:14:09 +0800

    String equalit and inequality operators

---
+++ string_equality_inequality.rb
@@ -0,0 +1,27 @@
+a = "Hello"
+b = "hello"
+c = "Hello"
+
+p a == "zebra"
+p a != "zebra"
+
+p a == b
+p a == c
+p b == c
+p a == a
+
+p a != b
+p a != c
+
+p "Apple" < "Banana"
+p "hello" < "help"
+
+p "A" < "a"
+p "Z" < "a"
+
+p "Help" < "banana"
+p "Help" < "Banana"
+
+p true == true
+p true == false
+p false == false
