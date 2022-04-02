d255b11b11842892336dc781450f18ea9e7a8753
kmandawe <kdmandawe06@yahoo.com.ph>
2022-04-02 11:54:48 +0800

    Assignments and Reassignments

---
+++ assignments_reassignments.rb
@@ -0,0 +1,44 @@
+a = 10
+p a
+
+a =  a + 5
+p a
+
+a += 5
+p a
+
+b = 100
+p b
+b = b - 50
+p b
+b -= 50
+p b
+
+c = 3
+p c
+c = c * 4
+p c
+c *= 4
+p c
+
+
+d = 50
+p d
+d = d / 5
+p d
+d /= 5
+p d
+
+e = 35
+p e
+e = e % 6
+p e
+e %= 2
+p e
+
+f = 5
+p f
+f = f ** 2
+p f
+f **= 2
+p f
