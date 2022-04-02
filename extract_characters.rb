bb887ecc507d0635cb1d5c052862680e6d886610
kmandawe <kdmandawe06@yahoo.com.ph>
2022-04-02 18:45:24 +0800

    Extract single character from Strings

---
+++ extract_characters.rb
@@ -0,0 +1,24 @@
+story = "Once upon a time in a land far, far away"
+
+p story.length
+
+p story[3]
+p story[2]
+p story[4]
+
+p story[-1]
+p story[-2]
+p story[-20]
+
+p story[100]
+p story[1000]
+p story[-300]
+
+p story[3]
+p story.slice(3)
+
+p story[-4]
+p story.slice(-4)
+
+p story[450]
+p story.slice(450)
