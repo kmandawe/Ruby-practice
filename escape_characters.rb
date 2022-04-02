4905d32a4c7b67177745e67488b518892d3665f9
kmandawe <kdmandawe06@yahoo.com.ph>
2022-04-02 17:59:07 +0800

    Escape characters

---
+++ escape_characters.rb
@@ -0,0 +1,16 @@
+puts "Hello world"
+puts 'Hello worls'
+
+puts "Juliet said 'Goodbye' to Romeo"
+puts 'Juliet said "Goodbye" to Romeo'
+
+puts "Juliet said \"Goodbye\" to Romeo"
+puts 'Juliet said \'Goodbye\' to Romeo'
+
+puts "Let's add a line break\nright here"
+result =  "Let's add a line break\nright here"
+p result
+
+puts "\tInsert a tab right here"
+puts "This is normal"
+
