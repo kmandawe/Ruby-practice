42e882b3b475102707e87dd82f8ca2527a86c088
kmandawe <kdmandawe06@yahoo.com.ph>
2022-04-02 11:38:26 +0800

    Float methods

---
+++ float_methods.rb
@@ -0,0 +1,16 @@
+p 10.5.to_i.class
+
+p 10.5.floor
+p 10.5.ceil
+
+p 3.14159.round
+p 3.6.round
+p 3.14159.round(2)
+p 3.14159.round(3)
+p 3.14159.round(4)
+
+p 35.67.abs
+p -35.67.abs
+p 50.abs
+p -50.abs
+
