fdf32d34f2cfc6d7f70e5004afa0f1522b861828
kmandawe <kdmandawe06@yahoo.com.ph>
2022-04-06 09:27:03 +0800

    String .include? method

---
+++ string_include_method.rb
@@ -0,0 +1,18 @@
+name = "Snow White"
+
+p name.include?("S")
+p name.include?("h")
+p name.include?("s")
+p name.include?(" ")
+p name.include?("  ")
+
+p name.downcase.include?("s")
+p name.upcase.include?("OW")
+
+p name.downcase.include?("snow")
+p name.downcase.include?("rain")
+p name.downcase.include?("it")
+p name.downcase.include?("sit")
+
+
+
