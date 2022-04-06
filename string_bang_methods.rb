6d48471dd0c726cd2c24c9b67ec9014c822e0c98
kmandawe <kdmandawe06@yahoo.com.ph>
2022-04-06 09:21:26 +0800

    String bang methods

---
+++ string_bang_methods.rb
@@ -0,0 +1,24 @@
+word = "hello"
+p word.capitalize
+p word
+
+word = word.capitalize
+p word
+
+word = "hello"
+p word
+word.capitalize!
+p word
+
+word.upcase!
+p word
+
+word.downcase!
+p word
+
+word.reverse!
+p word
+
+word.swapcase!
+p word
+
