32a878c6e87bed5715bbd4200a57665e762bf402
kmandawe <kdmandawe06@yahoo.com.ph>
2022-04-06 09:10:45 +0800

    String case methods

---
+++ string_case_methods.rb
@@ -0,0 +1,18 @@
+puts "hello"
+puts "hello".capitalize
+puts "heLLo".capitalize
+puts "Hello".capitalize
+
+puts "hello world".capitalize
+puts "hello World".capitalize
+
+
+puts "boris123".upcase
+puts "blah blah blah".upcase
+
+lowercase = "I'm patient"
+uppercase = lowercase.upcase
+p uppercase
+
+p "BORIS".downcase
+p "boRIs".swapcase
