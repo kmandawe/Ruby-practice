b3a79c7720aed73c2bae4456c0caf3910b90ea95
kmandawe <kdmandawe06@yahoo.com.ph>
2022-04-07 09:01:41 +0800

    Intro to methods

---
+++ methods.rb
@@ -0,0 +1,11 @@
+def introduce_myself
+  puts "I am handsome"
+  puts "I am talented"
+  puts "I am brilliant"
+end
+
+introduce_myself
+introduce_myself
+introduce_myself
+introduce_myself
+introduce_myself
