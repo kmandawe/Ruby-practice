a96397f49750ce8b0cb34e5af877347b232a627a
kmandawe <kdmandawe06@yahoo.com.ph>
2022-04-07 09:15:27 +0800

    Parameters and Arguments

---
+++ method_parameters_arguments.rb
@@ -0,0 +1,14 @@
+def praise_person(name, age)
+  puts "#{name} is amazing"
+  puts "#{name} is charming"
+  puts "#{name} is talented"
+  puts "His age in 5 years will be #{age + 5}"
+end
+
+# praise_person "Boris"
+# praise_person "Dave"
+# praise_person "Pizza"
+
+praise_person("Boris", 25)
+praise_person("Dave", 32)
+praise_person("Pizza", 3)
