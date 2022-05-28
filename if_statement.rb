968384793d98beda4392aba8619ffb09c540dbe9
kmandawe <kdmandawe06@yahoo.com.ph>
2022-05-28 10:53:29 +0800

    The if statement

---
+++ if_statement.rb
@@ -0,0 +1,30 @@
+puts 5 < 7
+
+if 5 < 2
+  puts "That math statement is true!"
+end
+
+password = "blah"
+
+if password == "topsecret"
+  puts "Congrats, you've logged in to our system!"
+end
+
+word = "kangaroo"
+
+if word.length == 8
+  puts "That word has 8 letters!"
+end
+
+if word.include?("roo")
+  puts "Yup, your word has roo in it!"
+end
+
+if 6.odd?
+  puts "That number is odd!"
+end
+
+
+
+
+
