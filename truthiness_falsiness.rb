f07824195efa25528ae57526512e862adc23b01a
kmandawe <kdmandawe06@yahoo.com.ph>
2022-05-28 14:06:36 +0800

    Truthiness and Falsiness

---
+++ truthiness_falsiness.rb
@@ -0,0 +1,25 @@
+if 5
+  puts "That is true"
+end
+
+if nil
+  puts "Nil is true"
+end
+
+if 0
+  puts "Zero is true"
+end
+
+if -123
+  puts "Negative numbers is true"
+end
+
+if {}
+  puts "Hash is true"
+end
+
+if 5..10
+  puts "Ranges are true"
+end
+
+
