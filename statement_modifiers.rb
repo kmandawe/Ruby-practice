fb809189acebce3b364fe1ed4445ec6b80c3f13d
kmandawe <kdmandawe06@yahoo.com.ph>
2022-05-28 19:39:35 +0800

    Statement Modifiers/ Inline Modifiers

---
+++ statement_modifiers.rb
@@ -0,0 +1,21 @@
+number = 5000
+verified = true
+
+if number > 2500
+  puts "Huge numbers!"
+end
+
+puts "Huge numbers!" if number > 2500
+
+puts "Huge numbers!" if number > 2500 || verified
+
+puts
+
+x = 8
+
+unless x > 10
+  puts "x is NOT greater than 10"
+end
+
+puts "x is NOT greater than 10" unless x > 10
+
