ae1c5e8e2fd0f903ac8515d42e71a12e2cfe2f8f
kmandawe <kdmandawe06@yahoo.com.ph>
2022-06-02 09:03:26 +0800

    Alphabetical Ranges

---
+++ alphabetical_ranges.rb
@@ -0,0 +1,12 @@
+puts (1..10).first(3)
+
+alphabet = "a".."z"
+p alphabet.first(5)
+puts alphabet.first(5)
+puts alphabet.last(8)
+
+alphabet = "A".."Z"
+puts alphabet.first(5)
+
+alphabet = "A".."z"
+puts alphabet.first(40)
