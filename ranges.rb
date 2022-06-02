1e057c0003500803c172dbc794043bef164366ba
kmandawe <kdmandawe06@yahoo.com.ph>
2022-06-02 08:54:01 +0800

    Intro to Ranges

---
+++ ranges.rb
@@ -0,0 +1,20 @@
+nums = 1..5
+p nums
+puts nums
+print nums
+puts
+p nums.class
+
+nums = 1...5
+p nums
+
+nums = 1..5
+p nums.first
+p nums.last
+
+nums = 90...150
+p nums.first
+p nums.last
+
+p nums.first(4)
+p nums.last(4)
