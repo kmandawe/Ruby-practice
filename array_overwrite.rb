f5409efac520485c713cd5c0203ce4b4cd0921e9
kmandawe <kdmandawe06@gmail.com>
2022-06-03 12:01:14 +0800

    Overwrite array elements

---
+++ array_overwrite.rb
@@ -0,0 +1,30 @@
+
+fruits = ["Apple" , "Orange", "Grapes", "Banana"]
+
+p fruits
+fruits[1] = "Watermelon"
+p fruits
+
+fruits[-1] = "Bananas"
+p fruits
+
+p fruits[4]
+fruits[4] = "Raspberry"
+p fruits
+
+fruits[5] = "Strawberry"
+p fruits
+
+fruits[10] = "Kiwi"
+p fruits
+
+fruits[3, 2] = ["Canteloupe", "Dragonfruit"]
+p fruits
+
+fruits[0..2] = ["Blackberry", "Orange", "Pears"]
+p fruits
+
+fruits[0..3] = ["Blah"]
+p fruits
+
+
