2e33f564c35e616570db781c4532e87179390d5c
kmandawe <kdmandawe06@gmail.com>
2022-06-03 10:15:07 +0800

    Intro to Arrays

---
+++ array_intro.rb
@@ -0,0 +1,23 @@
+p []
+
+p [].class
+
+numbers = [4, 8, 15, 16, 23, 42]
+p numbers
+puts numbers
+
+toys = ["Teddy bear", "Super Soaker", "board game"]
+p toys
+
+things = [4, true, "Hello", 10.99]
+p things
+
+registrations = [true, true, false, true, false]
+p registrations
+
+students = [["Boris", 25, true],
+            ["Sally", 23, true],
+            ["Ingrid", 31]
+]
+puts students
+
