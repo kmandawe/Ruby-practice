cec2093eb935b2b9d4f4b1769fded030797c2f3d
kmandawe <kdmandawe06@gmail.com>
2022-06-03 23:51:50 +0800

    Array length, size, count

---
+++ array_length_size.rb
@@ -0,0 +1,14 @@
+p [1, 2, 3, 4, "Hello", 3.14].length
+p [1, nil, 2, 3, 4, "Hello", 3.14].length
+p [1, nil, 2, 3, 4, "Hello", 3.14].size
+
+p [].length
+p [].size
+
+p [1, 2, 3, 4, 2, 3, 2, 2, nil].count
+
+p [1, 2, 3, 4, 2, 3, 2, 2, nil].count(2)
+
+p [true, true, false, false, false, false, true].count(true)
+
+
