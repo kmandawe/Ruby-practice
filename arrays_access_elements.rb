f95a1a0a9905099566d4546624b39338842bc2b3
kmandawe <kdmandawe06@gmail.com>
2022-06-03 10:38:52 +0800

    Access elements by index position

---
+++ arrays_access_elements.rb
@@ -0,0 +1,15 @@
+
+fruits = ["Apple", "Orange", "Grape", "Banana"]
+p fruits.length
+p fruits[0]
+p fruits[1]
+p fruits[3]
+p fruits[10]
+p fruits[100]
+p fruits[5]
+
+p fruits[fruits.length - 1]
+p fruits[-1]
+
+p fruits[3]
+p fruits.[](3)
