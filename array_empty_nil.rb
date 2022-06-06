cfce688ffcaf64e8902bebe459b1dace7a91ed60
kmandawe <kdmandawe06@gmail.com>
2022-06-06 12:24:51 +0800

    The empty? and ni? methods on an Array

---
+++ array_empty_nil.rb
@@ -0,0 +1,17 @@
+puts [1, 2, 3].empty?
+puts [].empty?
+puts [].length == 0
+
+puts [false, false, false].empty?
+puts [nil, nil].empty?
+
+puts [false, false, false].nil?
+puts [nil, nil].nil?
+puts [].nil?
+
+
+# usage
+letters = ("a".."j").to_a
+character = letters[25]
+p character.nil?
+
