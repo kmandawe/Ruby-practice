abba71480544e439efb65beb398325ed75cac445
kmandawe <kdmandawe06@gmail.com>
2022-06-06 13:12:03 +0800

    Array equality and inequality

---
+++ arrays_equality.rb
@@ -0,0 +1,20 @@
+a = [1, 2, 3]
+b = [1, 2, 3, 4]
+c = [3, 2, 1]
+d = [1, 2, 3]
+
+p a == b
+p a == c
+p a == d
+
+
+p a != b # true
+p a != c # true
+p a != d # false
+
+
+a = ["Skittles", "Starbursts", "Snickers"]
+b = ["Skittles", "Starbursts", "snickers"]
+
+p a == b
+p a != b
