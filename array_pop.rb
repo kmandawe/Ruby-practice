1374aae40a9766312b70c2c917b95033c274fd06
kmandawe <kdmandawe06@gmail.com>
2022-06-06 12:58:03 +0800

    Array pop method

---
+++ array_pop.rb
@@ -0,0 +1,16 @@
+arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+p arr.length
+arr.pop
+p arr.length
+p arr
+arr.pop
+p arr
+
+last_item = arr.pop
+p arr
+p last_item
+
+two_items = arr.pop(5)
+p arr
+
+p two_items
