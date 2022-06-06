92d0856091a26a0fcdd6b279db5671223430d8a7
kmandawe <kdmandawe06@gmail.com>
2022-06-06 13:23:45 +0800

    Convert Ranges to Array

---
+++ array_convert_range.rb
@@ -0,0 +1,15 @@
+letters_range = "A".."T"
+p letters_range
+letters_array = letters_range.to_a
+p letters_array.class
+
+p letters_array[5]
+
+numbers_range = 415..450
+p numbers_range
+p numbers_range.to_a
+numbers_array = numbers_range.to_a
+p numbers_array[-1]
+
+p numbers_array[10..15]
+
