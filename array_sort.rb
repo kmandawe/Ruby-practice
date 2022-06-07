3c3b97b2501bd075239f1defd5e8bf73fe033e9a
kmandawe <kdmandawe06@gmail.com>
2022-06-07 12:07:54 +0800

    Array sort method

---
+++ array_sort.rb
@@ -0,0 +1,16 @@
+numbers = [5, 13, 1, -2, 8]
+words = ["caterpillar", "kangaroo", "apple", "zebra"]
+
+p numbers.sort
+p words.sort
+
+p numbers
+p words
+
+p numbers.sort!
+p words.sort!
+
+p numbers
+p words
+
+
