b55dd1a466f82f31d159a87b64fcc8317bca0ba0
kmandawe <kdmandawe06@gmail.com>
2022-06-07 11:49:30 +0800

    Iteration over Array with while or until Loops

---
+++ array_while_until.rb
@@ -0,0 +1,15 @@
+animals = ["Lion", "Zebra", "Baboon", "Cheetah"]
+
+i = 0
+while i < animals.length
+  puts animals[i]
+  i += 1
+end
+
+puts
+
+i = 0
+until i == animals.length
+  puts animals[i]
+  i += 1
+end
