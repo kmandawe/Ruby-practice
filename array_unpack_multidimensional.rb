283f16ce11e106b445f7c952f7427e506eaf8948
kmandawe <kdmandawe06@gmail.com>
2022-06-07 14:31:06 +0800

    Unpack a multidimensional array

---
+++ array_unpack_multidimensional.rb
@@ -0,0 +1,16 @@
+users = [["Bob", 25, "Male"], ["Susan", 48, "Female"], ["Larry", 62, "Male"]]
+
+p users[0]
+p users[1]
+p users[1][2]
+
+bob, susan, larry = users
+p bob
+p susan
+p larry
+
+
+
+
+
+
