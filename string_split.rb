33e15571a133fac2f3a70801a1154f928f106550
kmandawe <kdmandawe06@gmail.com>
2022-06-09 10:15:01 +0800

    String .split method

---
+++ string_split.rb
@@ -0,0 +1,10 @@
+sentence = "Hi, my name is Boris. There are spaces here!"
+
+p sentence.split
+p sentence.split
+
+p sentence.split(".")
+
+words = sentence.split(" ")
+
+words.each { |word| puts word.length }
