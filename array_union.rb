339ae82eab0898e1c24334a764f56df836ee4874
kmandawe <kdmandawe06@gmail.com>
2022-06-25 16:01:11 +0800

    Array union (combine and remove duplicates)

---
+++ array_union.rb
@@ -0,0 +1,16 @@
+p [1, 2, 3] | [3, 4, 5]
+p [1, 1, 2, 3] | [3, 4, 4, 5]
+p [1, 1, 2, 3] | [3, 4, 4, 5] | [4, 5, 6, 7]
+p [1, 1, 2, 3].|([3, 4, 4, 5])
+
+a = [1, 1, 2, 3]
+b = [3, 4, 4, 5]
+
+def custom_union(arr1, arr2)
+  arr1.dup.concat(arr2).uniq
+end
+
+p custom_union(a, b)
+p a
+p b
+
