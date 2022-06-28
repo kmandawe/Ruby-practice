245acd926392b9512a75a7f752b5bad8503ed33d
kmandawe <kdmandawe06@gmail.com>
2022-06-28 12:07:40 +0800

    .merge method to Combine Hashes

---
+++ hash_merge.rb
@@ -0,0 +1,23 @@
+market = { garlic: "3 cloves", tomatoes: "5 batches", milk: "10 gallons" }
+kitchen = { bread: "2 loaves", yogurt: "20 cans", milk: "100 gallons" }
+
+p market.merge(kitchen)
+
+p kitchen.merge(market)
+
+p kitchen
+kitchen.merge!(market)
+p kitchen
+
+def custom_merge(hash1, hash2)
+  new_hash = hash1.dup
+  hash2.each do |key, value|
+    new_hash[key] = value
+  end
+  new_hash
+end
+
+market = { garlic: "3 cloves", tomatoes: "5 batches", milk: "10 gallons" }
+kitchen = { bread: "2 loaves", yogurt: "20 cans", milk: "100 gallons" }
+
+p custom_merge(market, kitchen)
