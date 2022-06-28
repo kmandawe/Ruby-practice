c0d35b6639443bbf4803dd2670f8a52f571abf5d
kmandawe <kdmandawe06@gmail.com>
2022-06-28 11:47:18 +0800

    The .delete Method on a Hash

---
+++ hash_delete.rb
@@ -0,0 +1,10 @@
+superheroes = { spiderman: "Peter Parker", superman: "Clark Kent",
+                batman: "Bruce Wayne" }
+
+p superheroes
+
+removed = superheroes.delete(:spiderman)
+superheroes.delete(:batman)
+
+p superheroes
+p removed
