0a64b71e7511b7a1b2e78369edaddaec6a961468
kmandawe <kdmandawe06@gmail.com>
2022-06-28 10:54:24 +0800

    Convert Hash to Array and Vice Versa

---
+++ hash_to_array.rb
@@ -0,0 +1,14 @@
+spice_girls = { scary: "Melanie Brown",
+                sporty: "Melanie Chisholm",
+                baby: "Emmma Bunton",
+                ginger: "Geri Halliwell",
+                posh: "Victoria Beckham" }
+
+p spice_girls.to_a
+p spice_girls.to_a.flatten
+
+power_rangers = [
+  [:red, "Jason"], [:black, "Zack"], [:blue, "Billy"], [:yellow, "Trini"], [:pink, "Kimberly"]
+]
+
+puts power_rangers.to_h
