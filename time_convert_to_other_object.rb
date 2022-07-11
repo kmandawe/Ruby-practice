9797d339e202f7f82442f81b5b269edd3b070ee0
kmandawe <kdmandawe06@gmail.com>
2022-07-11 13:26:21 +0800

    Convert Time Object to Other Objects

---
+++ time_convert_to_other_object.rb
@@ -0,0 +1,13 @@
+someday = Time.new(2000, 2, 15)
+p someday
+p someday.yday
+p someday.wday
+p someday.mday
+
+puts someday.to_s
+puts someday.ctime
+
+puts someday.ctime.class
+
+p someday.to_a
+p someday.to_a.class
