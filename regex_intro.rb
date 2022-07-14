19fcde161b1c00b7433f4d849f21ed337a0ade74
kmandawe <kdmandawe06@gmail.com>
2022-07-14 13:24:37 +0800

    Intro to Regular Expressions

---
+++ regex_intro.rb
@@ -0,0 +1,20 @@
+puts //.class
+
+phrase = 'The Ruby Programming Language is amazing.'
+
+puts phrase =~ /T/
+puts phrase =~ /R/
+puts phrase =~ /m/
+
+puts phrase =~ /!/
+
+puts phrase =~ /./
+puts phrase =~ / /
+p phrase =~ /x/
+
+p phrase =~ /Ru/
+p phrase =~ /ing/
+
+p phrase =~ /anguage/
+p phrase =~ /an/
+
