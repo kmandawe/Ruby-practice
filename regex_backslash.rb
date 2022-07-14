90ec50edd84d70158c275f64bad75953bece6ad9
kmandawe <kdmandawe06@gmail.com>
2022-07-14 14:12:25 +0800

    The Backslash

---
+++ regex_backslash.rb
@@ -0,0 +1,19 @@
+paragraph = 'This is my essay. I deserve an A. I rank it a 5 out of 5.'
+
+puts paragraph.scan(/\./)
+
+p paragraph.scan(/d/)
+
+p paragraph.scan(/\d/)
+
+p paragraph.scan(/\D/)
+
+p paragraph.scan(/\s/).length
+
+p paragraph.scan(/\s+/)
+
+p paragraph.scan(/\S/)
+
+
+
+
