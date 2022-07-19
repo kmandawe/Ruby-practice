aa146e0082fc560572105eb933ef049fef966885
kmandawe <kdmandawe06@gmail.com>
2022-07-19 10:17:24 +0800

    Review of the .class Method on Ruby Objects

---
+++ class_review.rb
@@ -0,0 +1,17 @@
+puts 3.14.class
+puts 99.99.class
+puts 0.95.class
+puts -13.253.class
+
+puts 3.class == 3.14.class
+
+puts "Hello world".class
+puts [1, 2, 3].class
+puts Hash.new(0).class
+puts true.class
+puts false.class
+puts nil.class
+puts (0..9).class
+puts //.class
+puts Proc.new {}.class
+puts Time.new.class
