4771651d8cd0d5fc5a3ad22cc03479d70b62791b
kmandawe <kdmandawe06@gmail.com>
2022-07-19 13:29:07 +0800

    Object Aliases

--- classes/gadget.rb
+++ classes/gadget.rb
@@ -1,31 +1,53 @@
 class Gadget
 end
 
-puts Gadget.superclass
-puts Gadget.superclass.superclass
+# # puts Gadget.superclass
+# # puts Gadget.superclass.superclass
+# #
+# # phone = Gadget.new
+# # laptop = Gadget.new
+# # microwave = Gadget.new
+# #
+# # puts phone
+# # puts laptop
+# # puts microwave
+# #
+# # puts phone.class
+# # puts laptop.class
+# # puts microwave.class
+# #
+# # puts phone.is_a?(Gadget)
+# # puts phone.is_a?(Object)
+# # puts phone.is_a?(BasicObject)
+#
+# puts phone.respond_to?(:class)
+# puts phone.respond_to?(:methods)
+# puts phone.respond_to?(:is_a?)
+# puts phone.respond_to?(:respond_to?)
+# puts phone.respond_to?(:length)
+#
+# puts phone.methods.sort
 
-phone = Gadget.new
-laptop = Gadget.new
-microwave = Gadget.new
+shiny = Gadget.new
+flashy = Gadget.new
 
-puts phone
-puts laptop
-puts microwave
+p shiny
+p flashy
 
-puts phone.class
-puts laptop.class
-puts microwave.class
+p shiny.object_id
+p flashy.object_id
 
-puts phone.is_a?(Gadget)
-puts phone.is_a?(Object)
-puts phone.is_a?(BasicObject)
+p shiny == flashy
+p shiny.object_id == flashy.object_id
 
-puts phone.respond_to?(:class)
-puts phone.respond_to?(:methods)
-puts phone.respond_to?(:is_a?)
-puts phone.respond_to?(:respond_to?)
-puts phone.respond_to?(:length)
+glossy = shiny
+p glossy.object_id
+p shiny.object_id
+p glossy.object_id == shiny.object_id
+p glossy == shiny
 
-puts phone.methods.sort
+p shiny == flashy
+p glossy == flashy
+
 
 
