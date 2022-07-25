13d92330a56c2dfda1ea4472221ba4592897c16a
kmandawe <kdmandawe06@gmail.com>
2022-07-25 12:14:35 +0800

    Mixins II - Custom Mixins

---
+++ mixins_part2.rb
@@ -0,0 +1,27 @@
+module Purchaseable
+  def purchase(item)
+    "#{item} has been purchased!"
+  end
+end
+
+class Bookstore
+  include Purchaseable
+end
+
+class Supermarket
+  include Purchaseable
+end
+
+barnes_and_noble = Bookstore.new
+p barnes_and_noble.purchase('Atlas Shrugged')
+
+shoprite = Supermarket.new
+p shoprite.purchase("Ice cream")
+
+class CornerMart < Supermarket
+end
+
+quickstop = CornerMart.new
+p quickstop.purchase("Slim Jim")
+
+
