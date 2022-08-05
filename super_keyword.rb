53bdf2f20c4bfa79d8a083a97d0d268357082578
kmandawe <kdmandawe06@gmail.com>
2022-08-05 14:00:18 +0800

    The super Keyword II

---
+++ super_keyword.rb
@@ -0,0 +1,31 @@
+
+class Car
+  attr_reader :maker
+  def initialize(maker)
+    @maker = maker
+  end
+
+  def drive
+    "Room! Room!"
+  end
+end
+
+class FireTruck < Car
+
+  attr_reader :sirens
+
+  def initialize(maker, sirens)
+    super(maker)
+    @sirens = sirens
+  end
+
+  def drive(speed)
+    super() + " Beep! Beep! I'm driving at #{speed} miles per hour"
+  end
+end
+
+ft = FireTruck.new("Ford", 4)
+p ft.drive(45)
+
+puts ft.maker
+puts ft.sirens
