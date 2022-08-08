ef918e9c035ed3b0ce22fb9c5a1cc582b870df3d
kmandawe <kdmandawe06@gmail.com>
2022-08-08 08:54:48 +0800

    Hash as initialize Argument II

--- candidate.rb
+++ candidate.rb
@@ -1,18 +1,26 @@
 class Candidate
   attr_accessor :name, :age, :occupation, :hobby, :birthplace
 
-  def initialize(name, details)
+  def initialize(name, details = {})
+    defaults = { age: 35, occupation: "Candidate", hobby: "Running for office" }
+    defaults.merge!(details)
+
     @name = name
-    @age = details[:age]
-    @occupation = details[:occupation]
-    @hobby = details[:hobby]
-    @birthplace = details[:birthplace]
+    @age = defaults[:age]
+    @occupation = defaults[:occupation]
+    @hobby = defaults[:hobby]
+    @birthplace = defaults[:birthplace]
   end
 end
 
 info = { age: 53, occupation: "Banker", hobby: "Fishing", birthplace: "Kentucky" }
 senator = Candidate.new("Mr. Smith", info)
 p senator.occupation
+p senator.hobby
+p senator.name
+
+senator = Candidate.new("Mr. Smith", hobby: "Horror movies", occupation: "Popcorn vendor")
+p senator.name
 p senator.hobby
 p senator.name
 
